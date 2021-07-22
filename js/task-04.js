f1356d826e4f5eeb878b2b7aa9ce2c6369db5cb6
Bohdan Sulimovsky <71587418+Sulimovsky@users.noreply.github.com>
2021-07-22 14:18:16 +0300

    Начал тз 4

---
+++ js/task-04.js
@@ -0,0 +1,10 @@
+const refs = {
+    decrement: document.querySelector('[data-action="decrement"]'),
+    increment: document.querySelector('[data-action="increment"]'),
+    value: document.querySelector('#value'),
+};
+
+console.log(refs);
+
+
+
