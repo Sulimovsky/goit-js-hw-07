0c6b368340dfa1863c760770ce3c9d3b4590a5c3
Bohdan Sulimovsky <71587418+Sulimovsky@users.noreply.github.com>
2021-07-22 12:35:33 +0300

    ТЗ 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,22 @@
-//
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+];
+
+const ulEl = document.querySelector('#ingredients');
+
+function createElement (element) {
+    return element.map(el => {
+        const liEl = document.createElement('li'); 
+        liEl.textContent = el;
+
+        return liEl;
+    });
+};
+
+const result = createElement(ingredients);
+ulEl.append(...result);
