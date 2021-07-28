e15d86ae44773bc52d2439a558fddf1e28b18f99
NilsRondhuis <84584712+NilsRondhuis@users.noreply.github.com>
2021-07-28 18:47:29 +0300

    Внест изминения

--- js/task-04.js
+++ js/task-04.js
@@ -10,13 +10,11 @@ refs.increment.addEventListener('click', increment);
 refs.decrement.addEventListener('click', decrement);
 
 function increment () {
-    let value = +counterValue ++;
-    refs.value.textContent = value;
+    +refs.value.textContent++;
 }
 
 function decrement () {
-    let value = +counterValue --;
-    refs.value.textContent = value;
+    refs.value.textContent--;
 }
 
 
